1b7f0466e62e912f657fb753765bcf9dc5773abb
JoaquinGomez1 <joaquingomez3098@gmail.com>
2020-12-17 18:42:59 -0300

    Responsive landing page

--- client/src/components/pages/Landing.jsx
+++ client/src/components/pages/Landing.jsx
@@ -6,8 +6,8 @@ import { FadeInOut } from "../Ui/FramerMotion";
 export default function Landing() {
   return (
     <FadeInOut className='w-screen pt-32 bg-indigo-800'>
-      <div className='landing-container max-w-screen-xl relative mx-auto px-8  h-100'>
-        <div className='left text-left w-1/2 absolute h-full left-0 px-20 text-white'>
+      <div className='landing-container max-w-screen-xl flex mx-auto px-8  h-100'>
+        <div className='left text-left w-1/2 px-20 text-white'>
           <div className='text-container -mt-16 h-full flex flex-col text-left justify-center'>
             <h3 className='opacity-80 text-2x1'> Choose where to travel </h3>
             <h3 className='text-3x1 text-bold text-yellow-400 text-shadow my-4'>
@@ -19,7 +19,7 @@ export default function Landing() {
             </div>
           </div>
         </div>
-        <div className='right w-1/2 absolute h-full right-0'>
+        <div className='right w-1/2 h-full '>
           <img
             className='w-full '
             src={process.env.PUBLIC_URL + "/img/undraw_adventure_4hum.svg"}
